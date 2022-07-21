73972a006182d66432791af592eb4d3f9a87a1b0
dev out <76041616+zmffjq0@users.noreply.github.com>
2022-07-22 00:46:42 +0900

    [Silver V] Title: K번째 수, Time: 3752 ms, Memory: 646912 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.py
@@ -0,0 +1,50 @@
+import sys
+input = sys.stdin.readline
+
+'''
+간편하지만 메모리초과
+'''
+# def quick_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+    
+#     pivot = arr[len(arr) // 2]
+#     lesser_arr, equal_arr, greater_arr = [], [], []
+#     for num in arr:
+#         if num < pivot:
+#             lesser_arr.append(num)
+#         elif num > pivot:
+#             greater_arr.append(num)
+#         else:
+#             equal_arr.append(num)
+    
+#     return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+def quick_sort(arr):
+    def sort(low, high):
+        if high <= low:
+            return
+
+        mid = partition(low, high)
+        sort(low, mid - 1)
+        sort(mid, high)
+
+    def partition(low, high):
+        pivot = arr[(low + high) // 2]
+        while low <= high:
+            while arr[low] < pivot:
+                low += 1
+            while arr[high] > pivot:
+                high -= 1
+            if low <= high:
+                arr[low], arr[high] = arr[high], arr[low]
+                low, high = low + 1, high - 1
+        return low
+
+    return sort(0, len(arr) - 1)
+
+N, K = map(int, input().split())
+
+arr = list(map(int, input().split()))
+quick_sort(arr)
+
+print(arr[K - 1])
